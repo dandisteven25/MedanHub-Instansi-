51ed42aa32c4564414b8011bdbd8eeb07904749e
dandisteven25 <dandisteven21@gmail.com>
2021-08-09 13:58:26 +0700

    add isLoggin account

--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomePage implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private navCtrl: NavController) { }
 
   ngOnInit() {
+    if(this.authService.isLoggedIn){
+      this.navCtrl.navigateRoot("/home/laporan")
+    }
   }
 
 }
